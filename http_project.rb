613be1459b42b37aed8e44bf303b8a92949235e3
Taylor Moore <Tmoore2272@gmail.com>
2015-11-04 11:06:04 -0700

    refactored

--- http_project.rb
+++ http_project.rb
@@ -4,7 +4,7 @@ require './date_time'
 require './output'
 require './word_validation'
 require './game'
-require './path'
+require './server'
 
 tcp_server = TCPServer.new(9292)
 request_count = 0
@@ -20,51 +20,21 @@ loop do
   end
 
   show = Output.new(client)
-  date = Date_time.new.date_format
-  reader = Reader.new(request_lines)
-  info = reader.read
-  # word_exist = reader.word_exist(request_lines)
+  parser = Parsers.new(request_lines)
+
 
   puts "Got this request: (#{request_count})"
   puts "Sending response."
 
-  case reader.find_verb
-  when "GET"
-    if reader.path == "/"
-      response = "<pre>#{info}\n #{request_lines.inspect} </pre>"
-      show.output(response)
-    elsif reader.path == "/hello"
-      response = "<pre> Hello world (#{request_count})\r\n #{info}\r\n </pre>"
-      show.output(response)
-    elsif reader.path == "/datetime"
-      response = "<pre>#{date}\r\n #{info}</pre>"
-      show.output(response)
-    elsif reader.path == "/word_search"
-      words = Word_validation.new(reader.values)
-      word_output = words.word_output
-      response = "<pre>#{word_output} \r\n #{info} </pre>"
-      show.output(response)
-    elsif reader.path == "/game"
-      # game = Game.new
-      response = "<pre> #{game.check_guess(reader.values.join.to_i)} \r\n #{info}</pre>"
-      show.output(response)
-    elsif reader.path == "/shutdown"
-      response = "<pre> Total requests: #{request_count}</pre>"
-      show.output(response)
-      client.close
-      break
-    end
-  when "POST"
-    if reader.path == "/start_game"
-      game = Game.new
-      response = "<pre> START GAME WOOT! \r\n #{info}</pre>"
-      show.output(response)
-    elsif reader.path == "/game"
-        response = "<pre> #{game.check_guess(reader.values.join.to_i)} \r\n #{info}</pre>"
-        show.output(response)
-    end
+  case parser.path
+  when "/" then response = parser.home
+  when "/hello" then response = parser.hello(request_count)
+  when "/datetime" then response = parser.date_time
+  when "/word_search" then response = parser.word_search
+  when "/shutdown" then response = parser.shutdown
   end
+
+      show.output(response)
+
   # unless reader.path(request_lines "/favicon.ico")
-
-
 end
