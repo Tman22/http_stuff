f6db0720fe3970703d6e66d243bbdce7e1a57aa9
James Crockett <jecrockett@gmail.com>
2015-11-04 21:18:04 -0700

    started two test files

---
+++ output_test.rb
@@ -0,0 +1,11 @@
+require 'minitest/autorun'
+require './output'
+#require './lib/output'
+
+class OutputTest < Minitest::Test
+
+  def test_output_object_exists
+    show = Output.new
+  end
+
+end
---
+++ server_test.rb
@@ -0,0 +1,9 @@
+require 'minitest/autorun'
+require './server'
+#require './lib/server'
+
+class ServerTest < Minitest::Test
+
+  
+
+end
